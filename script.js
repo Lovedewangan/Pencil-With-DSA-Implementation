985cdf6823d71e6209d53ec12e616ef7046aa699
Love Dewangan <lovedewangan9@gmail.com>
2024-09-29 19:24:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('wss://pencil-with-dsa-implementation.onrender.com');
 
 socket.onopen = function (event) {
     console.log('Connected to the WebSocket server');
